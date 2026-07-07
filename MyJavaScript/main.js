b12464b0bd0ffbc0c1d06ce26cb6e19fdf0b2b89
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-07 14:41:04 +0900

    constとletの違いについて学習しました。

--- MyJavaScript/main.js
+++ MyJavaScript/main.js
@@ -4,10 +4,16 @@
 // console.log(150 * 130 * 1.1);
 // console.log(150 * 140 * 1.1);
 
+// 変数
+let price = 150;
 // 定数
-const price = 150;
 const rate = 1.1;
 
+console.log(price * 120 * rate);
+console.log(price * 130 * rate);
+console.log(price * 140 * rate);
+
+price = 151;
 console.log(price * 120 * rate);
 console.log(price * 130 * rate);
 console.log(price * 140 * rate);
