02c929779b6810275994b145b21a4a8ab0268c1b
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-07 15:26:27 +0900

    forの中でforを使ってみよう

--- MyJavaScript/main.js
+++ MyJavaScript/main.js
@@ -1,5 +1,10 @@
 "use strict";
 
-for (let i = 1; i <= 10; i++) {
-  console.log(`${i}: Hello`);
+const rate = 1.1;
+
+for (let price = 150; price <= 160; price++) {
+  console.log(`Price: ${price}`);
+  for (let amount = 120; amount <= 140; amount += 10) {
+    console.log(price * amount * rate);
+  }
 }
