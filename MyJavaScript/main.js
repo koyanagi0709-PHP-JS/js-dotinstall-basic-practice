a246e32bd4497130c79f172aa914e9793ed62446
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-07 14:55:22 +0900

    テンプレートリテラルについて学習しました。

--- MyJavaScript/main.js
+++ MyJavaScript/main.js
@@ -1,5 +1,9 @@
 "use strict";
 
-console.log("It's a pen.");
-console.log("It' a pen.");
-console.log("It' \na \npen.");
+// My name is Taro Yamada, call me Taro!
+
+const fname = "Taro";
+const lname = "Yamada";
+
+console.log("My name is " + fname + " " + lname + ", call me " + fname + "!");
+console.log(`My name is ${fname} ${lname}, call me ${fname}!`);
