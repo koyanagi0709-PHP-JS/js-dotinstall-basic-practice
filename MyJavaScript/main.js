48705d281def4eb8f2c052324214d4572fc2f52a
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-07 15:11:25 +0900

    else if ... で条件をつなげてみよう

--- MyJavaScript/main.js
+++ MyJavaScript/main.js
@@ -2,11 +2,13 @@
 
 // console.log(50 + "20");
 
-const score = prompt("Score?");
+const score = Number(prompt("Score?"));
 // console.log(Number(score) + 10);
 
 if (score >= 90) {
   console.log("A!");
+} else if (score >= 70) {
+  console.log("B!");
 } else {
-  console.log("Not A!");
+  console.log("C!");
 }
