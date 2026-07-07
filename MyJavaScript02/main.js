4c90f3c92ac215d836cda06a40c62edba77defc7
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 08:59:14 +0900

    配列の値を集計してみよう

--- MyJavaScript02/main.js
+++ MyJavaScript02/main.js
@@ -2,9 +2,13 @@
 
 {
   const scores = [70, 90, 80, 85];
-
+  let sum = 0;
   scores.forEach((score, index) => {
     // console.log(score);
+    sum += score;
     console.log(`${index}: ${score}`);
   });
+
+  console.log(`Sum: ${sum}`);
+  console.log(`Average: ${sum / scores.length}`);
 }
