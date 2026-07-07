910805652ba01c171ee4dc119f2390b3a4960816
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-07 15:22:38 +0900

    forで反復処理をしてみよう

--- MyJavaScript/main.js
+++ MyJavaScript/main.js
@@ -1,18 +1,5 @@
 "use strict";
 
-const color = prompt("Color?");
-
-switch (color) {
-  case "red":
-    console.log("Stop!");
-    break;
-  case "yellow":
-    console.log("Slow down!");
-    break;
-  case "blue":
-  case "green":
-    console.log("Go!");
-    break;
-  default:
-    console.log("Wrong color");
+for (let i = 1; i <= 10; i++) {
+  console.log(`${i}: Hello`);
 }
