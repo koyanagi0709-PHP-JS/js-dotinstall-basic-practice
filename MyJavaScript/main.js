26770e4d68d6402d11a81002445921b12bca5809
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-07 14:32:18 +0900

    コメントについて学習しました。

--- MyJavaScript/main.js
+++ MyJavaScript/main.js
@@ -1,3 +1,15 @@
 "use strict";
 
-console.log(100);
+/**
+ * JavaScriptにおける数値の表現
+ * @koyanagi
+ */
+
+// console.log(100);
+// console.log(-10);
+// console.log(2.5);
+
+// 大きな数値、小さな数値の表現
+
+console.log(1.2e4); /*1.2かける10の4乗をかける = 12000 */
+console.log(1.2e-4); // 1.2かける10の-4乗 = 0.00012
