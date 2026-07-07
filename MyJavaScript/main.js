df1596bc7f18fcc0d1340a493e4ccfac7087851e
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-07 16:07:42 +0900

    関数を作ってみよう

--- MyJavaScript/main.js
+++ MyJavaScript/main.js
@@ -1,6 +1,10 @@
 "use strict";
 
 {
-  let x = 10;
-  console.log(x);
+  function double(num) {
+    // 仮引数
+    return num * 2;
+  }
+  console.log(double(10)); // 実引数
+  console.log(double(4) * 3); // 実引数
 }
