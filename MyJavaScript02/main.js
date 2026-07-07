17d6899f8932d928603eef7ba2fef55fffb20251
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 08:51:02 +0900

    配列と反復処理を組み合わせよう

--- MyJavaScript02/main.js
+++ MyJavaScript02/main.js
@@ -3,6 +3,9 @@
 {
   const scores = [70, 90, 80, 85];
 
-  scores.push(65, 72);
-  console.log(scores);
+  // console.log(scores[0]);
+  scores.push(77, 88);
+  for (let i = 0; i < scores.length; i++) {
+    console.log(scores[i]);
+  }
 }
