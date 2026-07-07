3d5a6153f64f12ae9788010df8bcfd5ac2337503
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-07 16:14:12 +0900

    返り値のない関数を作ろう

--- MyJavaScript/main.js
+++ MyJavaScript/main.js
@@ -1,10 +1,16 @@
 "use strict";
 
 {
-  function double(num) {
-    // 仮引数
-    return num * 2;
+  // function sum(a, b) {
+  //   return a + b;
+  // }
+
+  // console.log(sum(300, 700));
+
+  function sum(a, b) {
+    console.log(a + b);
+    // return a + b;
   }
-  console.log(double(10)); // 実引数
-  console.log(double(4) * 3); // 実引数
+
+  sum(300, 700);
 }
