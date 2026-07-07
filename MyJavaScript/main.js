fa12d3ff7329cf8a91168afb8a05143416bdc224
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-07 14:39:27 +0900

    変数について学習しました。

--- MyJavaScript/main.js
+++ MyJavaScript/main.js
@@ -1,10 +1,13 @@
 "use strict";
 
-console.log(10 + 3); // 13
-console.log(10 - 3); // 7
-console.log(10 * 3); // 30
-console.log(10 ** 3); // 1000
-console.log(10 / 3); // 3.33333
-console.log(10 % 3); // 1
+// console.log(150 * 120 * 1.1);
+// console.log(150 * 130 * 1.1);
+// console.log(150 * 140 * 1.1);
 
-console.log((10 + 2) * 3); //36
+// 定数
+const price = 150;
+const rate = 1.1;
+
+console.log(price * 120 * rate);
+console.log(price * 130 * rate);
+console.log(price * 140 * rate);
