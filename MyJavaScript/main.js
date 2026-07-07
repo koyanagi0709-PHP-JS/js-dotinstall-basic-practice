d7a878b413fa3056dce29f6b62ff838f6b0702cc
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-07 15:35:57 +0900

    do ... while を使ってみよう

--- MyJavaScript/main.js
+++ MyJavaScript/main.js
@@ -1,11 +1,18 @@
 "use strict";
 
-// const command = Number(prompt("Menu 1,2,3 or 0 to exit"));
-// console.log(`Menu ${command} processed.`);
+// let command = Number(prompt("Menu 1,2,3 or 0 to exit"));
 
-let command = Number(prompt("Menu 1,2,3 or 0 to exit"));
+// while (command !== 0) {
+//   console.log(`Menu ${command} processed.`);
+//   command = Number(prompt("Menu 1,2,3 or 0 to exit"));
+// }
 
-while (command !== 0) {
-  console.log(`Menu ${command} processed.`);
+let command;
+do {
   command = Number(prompt("Menu 1,2,3 or 0 to exit"));
-}
+  if (command === 0) {
+    console.log("Exited");
+  } else {
+    console.log(`Menu ${command} processed.`);
+  }
+} while (command !== 0);
