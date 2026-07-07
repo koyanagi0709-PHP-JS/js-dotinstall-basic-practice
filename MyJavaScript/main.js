412dd8f8446bf6bbee836306415dfa9065e88afe
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-07 14:51:29 +0900

    文字列を表現してみよう

--- MyJavaScript/main.js
+++ MyJavaScript/main.js
@@ -1,23 +1,5 @@
 "use strict";
 
-// console.log(150 * 120 * 1.1);
-// console.log(150 * 130 * 1.1);
-// console.log(150 * 140 * 1.1);
-
-// 変数
-let price = 150;
-// 定数
-const rate = 1.1;
-
-console.log(price * 120 * rate);
-console.log(price * 130 * rate);
-console.log(price * 140 * rate);
-
-// price = 151;
-// price = price + 1;
-// price += 1;
-// price *= 1.5;
-price++;
-console.log(price * 120 * rate);
-console.log(price * 130 * rate);
-console.log(price * 140 * rate);
+console.log("It's a pen.");
+console.log("It' a pen.");
+console.log("It' \na \npen.");
