54aa08d9ddaeafcb72e07845e12fed22a0e02802
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-07 16:17:31 +0900

    returnの挙動を理解しよう

--- MyJavaScript/main.js
+++ MyJavaScript/main.js
@@ -1,16 +1,17 @@
 "use strict";
 
 {
-  // function sum(a, b) {
-  //   return a + b;
-  // }
+  function sum(a, b) {
+    return a + b;
+  }
 
   // console.log(sum(300, 700));
 
-  function sum(a, b) {
-    console.log(a + b);
-    // return a + b;
-  }
+  // function sum(a, b) {
+  //   console.log(a + b);
+  //   // return undefined;
+  // }
 
-  sum(300, 700);
+  // console.log(sum(300, 700));
+  console.log(sum(300, 700) * 3);
 }
