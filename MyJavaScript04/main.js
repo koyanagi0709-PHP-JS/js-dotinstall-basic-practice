1f8ccec4e63fc1febaad5444e2cc79e1964c4557
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 14:42:50 +0900

    日時の差を求めてみよう

--- MyJavaScript04/main.js
+++ MyJavaScript04/main.js
@@ -1,13 +1,10 @@
 "use strict";
 
 {
-  // 2000 4 11
-  const d = new Date(2000, 3, 11);
-  console.log(d);
-
-  // Unix Timestamp
-  console.log(d.getTime());
-
-  const d2 = new Date(d.getTime() + 1000);
-  console.log(d2);
+  // 2023 6 10
+  // new Date() - new Date(2023, 5, 10);
+  // const diff = new Date().getTime() - new Date(2023, 5, 10).getTime();
+  const diff = Date.now() - new Date(2023, 5, 10).getTime();
+  const days = Math.floor(diff / 1000 / 60 / 60 / 24);
+  console.log(days);
 }
