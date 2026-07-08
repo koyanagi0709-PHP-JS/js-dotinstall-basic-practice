734adb5324d5d6cab26dba3adf83ac7a30658d50
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 14:22:40 +0900

    JavaScript入門 日時操作編 #02 日時に関する情報を取得しよう

--- MyJavaScript04/main.js
+++ MyJavaScript04/main.js
@@ -4,4 +4,12 @@
   const d = new Date();
   console.log(d);
   console.log(d.toLocaleString());
+  console.log(d.getFullYear());
+  console.log(d.getMonth()); // 0, 1, 2
+  console.log(d.getDate());
+  console.log(d.getHours());
+  console.log(d.getMinutes());
+  console.log(d.getSeconds());
+  console.log(d.getMilliseconds());
+  console.log(d.getDay());
 }
