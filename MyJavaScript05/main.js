40cbed87cdbf9402269288d387e0d13614deaa26
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 14:56:20 +0900

    !で真偽値を反転させよう

--- MyJavaScript05/main.js
+++ MyJavaScript05/main.js
@@ -11,5 +11,16 @@
   const pElement = document.querySelector("p");
   const buttonElement = document.querySelector("button");
 
-  buttonElement.addEventListener("click", () => {});
+  buttonElement.addEventListener("click", () => {
+    if (isDevMode === true) {
+      pElement.textContent = "Dev Mode is Off.";
+      // isDevMode = false;
+      // isDevMode = !isDevMode;
+    } else {
+      pElement.textContent = "Dev Mode is On.";
+      // isDevMode = true;
+      // isDevMode = !isDevMode;
+    }
+    isDevMode = !isDevMode;
+  });
 }
