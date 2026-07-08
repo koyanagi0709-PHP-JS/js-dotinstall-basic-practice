a9f6fea9a06596dd4a8f7412b32228ef9d702e07
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 14:53:54 +0900

    真偽値を扱ってみよう

---
+++ MyJavaScript05/main.js
@@ -0,0 +1,15 @@
+"use strict";
+
+{
+  // true false
+  //let mode = 'dev';
+  //let mode = 'normal';
+  //let isDevMode = true;
+  //let isDevMode = false;
+
+  let isDevMode = true;
+  const pElement = document.querySelector("p");
+  const buttonElement = document.querySelector("button");
+
+  buttonElement.addEventListener("click", () => {});
+}
