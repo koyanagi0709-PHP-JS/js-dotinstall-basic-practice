927a525cc150b9e94bb1bcec853d116cac3b7f60
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 12:19:13 +0900

    フォーム部品の値にアクセスしてみよう

--- MyJavaScript03/main.js
+++ MyJavaScript03/main.js
@@ -2,15 +2,7 @@
 
 {
   document.querySelector("button").addEventListener("click", () => {
-    const liElement = document.createElement("li");
-    liElement.textContent = "Hanako";
-
-    // document.querySelector("ul").appendChild(liElement);
-    // document
-    //   .querySelector("ul")
-    //   .insertBefore(liElement, document.querySelector("#second"));
-    if (confirm("Sure?") === true) {
-      document.querySelector("#second").remove();
-    }
+    alert(document.querySelector("textarea").value);
+    document.querySelector("textarea").value = "";
   });
 }
