0f48d89ab8e3cac706a4035e5ca120e3e83957f5
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 11:28:57 +0900

    textContentを使ってみよう

--- MyJavaScript03/main.js
+++ MyJavaScript03/main.js
@@ -2,6 +2,11 @@
 
 {
   document.querySelector("button").addEventListener("click", () => {
-    console.log("Clicked");
+    // console.log("Clicked");
+    // document.querySelector("p").textContent = "こんにちは";
+    document.querySelector("p").textContent =
+      document.querySelector("button").textContent;
   });
+
+  console.log("Hello");
 }
