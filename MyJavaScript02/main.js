1c8f88b1ffca4045d4f93aed9d829e31fe4cde19
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 09:28:25 +0900

    プロパティを追加､削除してみよう

--- MyJavaScript02/main.js
+++ MyJavaScript02/main.js
@@ -2,9 +2,8 @@
 
 {
   const scores = { math: 80, english: 90 };
-  // console.log(scores['english']);
-  console.log(scores.english);
-  // scores['math'] = 88;
-  scores.math = 88;
+
+  scores.physics = 70;
+  delete scores.english;
   console.log(scores);
 }
