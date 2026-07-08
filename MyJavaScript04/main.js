407cbaa30cae834fbcc1097397fabb4269b14365
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 14:37:50 +0900

    getTime()の挙動を確認しよう

--- MyJavaScript04/main.js
+++ MyJavaScript04/main.js
@@ -2,14 +2,12 @@
 
 {
   // 2000 4 11
-  // const d = new Date(2000, 3, 11);
-  // 2002 2 ??
   const d = new Date(2000, 3, 11);
-  // const dBackup = new Date(d.getFullYear(), d.getMonth(), d.getDate());
-  const dBackup = new Date(d.getTime());
-  // d.setDate(23);
-  d.setDate(23);
+  console.log(d);
 
-  console.log(d.toLocaleString());
-  console.log(dBackup.toLocaleString());
+  // Unix Timestamp
+  console.log(d.getTime());
+
+  const d2 = new Date(d.getTime() + 1000);
+  console.log(d2);
 }
