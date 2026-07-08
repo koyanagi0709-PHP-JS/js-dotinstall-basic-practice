df10b69618f875c2b4ae35d33482665544f5e234
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 14:34:23 +0900

    日時データをコピーしよう

--- MyJavaScript04/main.js
+++ MyJavaScript04/main.js
@@ -5,8 +5,11 @@
   // const d = new Date(2000, 3, 11);
   // 2002 2 ??
   const d = new Date(2000, 3, 11);
+  // const dBackup = new Date(d.getFullYear(), d.getMonth(), d.getDate());
+  const dBackup = new Date(d.getTime());
   // d.setDate(23);
-  d.setDate(d.getDate() + 100);
+  d.setDate(23);
 
   console.log(d.toLocaleString());
+  console.log(dBackup.toLocaleString());
 }
