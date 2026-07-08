59bda1f5fb47cd884a1afbdbcafea5f1ee5bbe84
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 10:00:04 +0900

    オブジェクトの値を集計してみよう

--- MyJavaScript02/main.js
+++ MyJavaScript02/main.js
@@ -2,11 +2,16 @@
 
 {
   const scores = { math: 80, english: 90 };
+  let sum = 0;
+  scores.physics = 70;
 
   const entries = Object.entries(scores);
   // console.log(entries);
   entries.forEach((prop) => {
     // console.log(prop);
+    sum += prop[1];
     console.log(`${prop[0]}: ${prop[1]}`);
   });
+  console.log(`Sum: ${sum}`);
+  console.log(`Average: ${sum / entries.length}`);
 }
