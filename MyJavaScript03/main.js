38c44eb8d03aafd49f52fb375edbb61594d16ec0
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 11:46:57 +0900

    より複雑なセレクターを使ってみよう

--- MyJavaScript03/main.js
+++ MyJavaScript03/main.js
@@ -2,12 +2,12 @@
 
 {
   document.querySelector("button").addEventListener("click", () => {
-    // document.querySelector("li").textContent = "Changed!"; //最初の要素のみ
+    document.querySelector("#second").textContent = "Changed!"; //最初の要素のみ
     // document.querySelectorAll("li")[0].textContent = "Changed!";
     // document.querySelectorAll("li")[1].textContent = "Changed!";
     // document.querySelectorAll("li")[2].textContent = "Changed!";
 
-    document.querySelectorAll("li").forEach((li) => {
+    document.querySelectorAll(".target").forEach((li) => {
       li.textContent = "Changed!";
     });
   });
