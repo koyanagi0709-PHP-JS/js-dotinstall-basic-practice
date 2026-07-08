9b95bea3fcefec83c25fd19fc168fc9e6baf1ad3
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 15:18:30 +0900

    nullを使ってみよう

--- MyJavaScript05/main.js
+++ MyJavaScript05/main.js
@@ -1,16 +1,10 @@
 "use strict";
 
 {
-  // undefined
-  let x;
-  // console.log(x);
+  // null
 
-  const scores = [70, 90, 80];
-  // console.log(scores[4]);
-
-  const score = { math: 80, english: 90 };
-  // console.log(score.history);
-  if (score.history === undefined) {
-    console.log("History score not defined!");
+  const score = { math: 80, english: 90, physics: null };
+  if (score.physics === null) {
+    console.log("History score not null!");
   }
 }
