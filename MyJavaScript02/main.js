ae3076b3f2d09b6aa5e4e4fd47419f77acfd4c88
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 10:15:44 +0900

    splice()で配列を操作してみよう

--- MyJavaScript02/main.js
+++ MyJavaScript02/main.js
@@ -1,17 +1,15 @@
 "use strict";
 
 {
-  const scores = { math: 80, english: 90 };
-  let sum = 0;
-  scores.physics = 70;
+  const scores = [70, 90, 80, 85];
+  scores.splice(2, 0, 77, 88);
+  //[70,90,77,88,80,85]
+  const deleted = scores.splice(3, 1);
+  //[70,90,77,80,85]
+  // 80
+  scores.splice(2, 2, 30);
+  //[70,90,30,85]
 
-  const entries = Object.entries(scores);
-  // console.log(entries);
-  entries.forEach((prop) => {
-    // console.log(prop);
-    sum += prop[1];
-    console.log(`${prop[0]}: ${prop[1]}`);
-  });
-  console.log(`Sum: ${sum}`);
-  console.log(`Average: ${sum / entries.length}`);
+  console.log(scores);
+  console.log(deleted);
 }
