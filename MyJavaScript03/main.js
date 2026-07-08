5a7ae4500f0b465b1ea4449039ca0f41fbbf1aa1
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 11:38:29 +0900

    classListでスタイルを切り替えよう

--- MyJavaScript03/main.js
+++ MyJavaScript03/main.js
@@ -2,7 +2,13 @@
 
 {
   document.querySelector("button").addEventListener("click", () => {
-    document.querySelector("p").classList.add("pink-bg", "red-border");
-    document.querySelector("p").classList.remove("green-color");
+    // console.log(document.querySelector("p").classList.contains("pink-bg"));
+
+    // if (document.querySelector("p").classList.contains("pink-bg") === false) {
+    //   document.querySelector("p").classList.add("pink-bg");
+    // } else {
+    //   document.querySelector("p").classList.remove("pink-bg");
+    // }
+    document.querySelector("p").classList.toggle("pink-bg");
   });
 }
