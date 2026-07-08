d94d00c747dde3f1543a591796ab21eb67296fda
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 14:31:12 +0900

    日時を更新してみよう

--- MyJavaScript04/main.js
+++ MyJavaScript04/main.js
@@ -4,7 +4,9 @@
   // 2000 4 11
   // const d = new Date(2000, 3, 11);
   // 2002 2 ??
-  const d = new Date(2000, 2, 0);
+  const d = new Date(2000, 3, 11);
+  // d.setDate(23);
+  d.setDate(d.getDate() + 100);
 
   console.log(d.toLocaleString());
 }
