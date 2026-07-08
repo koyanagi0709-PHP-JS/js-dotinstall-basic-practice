6591b7042c045e7819849ebb2f4b59a2a78dde63
Hiroaki Koyanagi <koyanagi0709@softbank.ne.jp>
2026-07-08 11:58:52 +0900

    createElement()で要素を生成しよう

--- MyJavaScript03/main.js
+++ MyJavaScript03/main.js
@@ -2,13 +2,6 @@
 
 {
   document.querySelector("button").addEventListener("click", () => {
-    document.querySelector("#second").textContent = "Changed!"; //最初の要素のみ
-    // document.querySelectorAll("li")[0].textContent = "Changed!";
-    // document.querySelectorAll("li")[1].textContent = "Changed!";
-    // document.querySelectorAll("li")[2].textContent = "Changed!";
-
-    document.querySelectorAll(".target").forEach((li) => {
-      li.textContent = "Changed!";
-    });
+    const liElement = document.createElement("li");
   });
 }
